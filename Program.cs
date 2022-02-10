5499f2348bbba61a38ba5371344f4e5e27a2ab6a
nikita <sabaoth@ukr.net>
2022-02-10 19:51:01 +0200

    interface completed

--- Program.cs
+++ Program.cs
@@ -4,9 +4,36 @@ namespace ToDoList
 {
     class Program
     {
+        static string[,] task = { };
+        static string[,] completedTask = { };
+        static string[,] rejectedTask = { };
+        static string[] possibleInputs = { "add-item", "remove-item", "mark-as", "show", "exit" };
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        static void AskQuestion()
+        {
+            Console.WriteLine("This is your new To-Do list 3000");
+            Console.WriteLine("Please tell me, what are you planing to do today. \n The options are: ");
+            Console.WriteLine($"\"{possibleInputs[0]}\" - To add a new task");
+            Console.WriteLine($"\"{possibleInputs[1]}\" - To remove an item");
+            Console.WriteLine($"\"{possibleInputs[1]}\" - To mark some task as completed/rejected");
+            Console.WriteLine($"\"{possibleInputs[2]}\" - To see some completed/rejected tasks or all the tasks!");
+            Console.WriteLine($"\"{possibleInputs[3]}\" - To exit.");
+            string input = Console.ReadLine();
+            InputController(input);
+        }
+
+        static void InputController(string userInput)
+        {
+
+
+            if(Array.Exists(possibleInputs, x => x == userInput))
+            {
+
+            }
         }
     }
 }
